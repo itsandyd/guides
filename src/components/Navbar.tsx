6eb4db17dc9831af8d590a8cf88678a9c90a8e5e
pauseplayrepeat <mrsqueex@gmail.com>
2023-11-08 02:32:20 -0600

    building navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,28 +11,30 @@ const Navbar = async () => {
   const session = await getServerSession(authOptions)
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
-      <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
-        {/* logo */}
-        <Link href='/' className='flex gap-2 items-center'>
-          <Box className='h-8 w-8 sm:h-6 sm:w-6' />
-          <p className='hidden text-zinc-700 text-sm font-medium md:block'>Neat Guides</p>
-        </Link>
+      <div className='container max-w-7xl h-full mx-auto flex items-center justify-between'>
+  {/* logo */}
+  <Link href='/' className='flex gap-2 items-center'>
+    <Box className='h-8 w-8 sm:h-6 sm:w-6' />
+    <p className='hidden text-zinc-700 text-sm font-medium md:block'>Neat Guides</p>
+  </Link>
 
-        {/* search bar */}
-        {/* <SearchBar /> */}
-        <Link href="categories">
-          <Button variant="link">Guide Categories</Button>
-        </Link>
-        <Link href="twitch">
-          <Button variant="link">Streamers</Button>
-        </Link>
-        <Link href="https://github.com/neatgang/guides">
-          <GithubIcon />
-        </Link>
-        {/* actions */}
-        {/* <div className="flex"> */}
+  {/* search bar */}
+  {/* <SearchBar /> */}
+  <Link href="categories">
+    <Button variant="link">Guide Categories</Button>
+  </Link>
+  <Link href="twitch">
+    <Button variant="link">Streamers</Button>
+  </Link>
+  {/* actions */}
+  {/* <div className="flex"> */}
         {session?.user ? (
-          <UserAccountNav user={session.user} />
+          <>
+      <Link href="https://github.com/neatgang/guides">
+          <GithubIcon />
+      </Link>
+      <UserAccountNav user={session.user} />
+    </>
         ) : (
           <Link href='/sign-in' className={buttonVariants()}>
             Sign In
