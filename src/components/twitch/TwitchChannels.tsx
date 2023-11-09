e7a37348e1e9968f5339a161986319cf3aa3c255
pauseplayrepeat <mrsqueex@gmail.com>
2023-11-08 23:37:13 -0600

    updating

--- src/components/twitch/TwitchChannels.tsx
+++ src/components/twitch/TwitchChannels.tsx
@@ -14,16 +14,7 @@ type TwitchChannelsProps = {
   channels: TwitchChannel[];
 };
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
 const TwitchChannels: React.FC<TwitchChannelsProps> = ({ channels }) => {
-
-    const { data: channels, error } = useSWR('/api/twitch/channels', fetcher, { refreshInterval: 5000 });
-
-    if (error) return <div>Failed to load</div>
-    if (!channels) return <div>Loading...</div>
-
-
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
       {channels.map((channel: TwitchChannel, index: number) => (
