8b93faae9fce8c96d379ed0980c1285679ca786a
pauseplayrepeat <mrsqueex@gmail.com>
2023-11-08 20:00:24 -0600

    updated add streamer page

--- src/app/twitch/add/page.tsx
+++ src/app/twitch/add/page.tsx
@@ -1,8 +1,17 @@
 "use client"
 
 import { ChannelForm } from "@/components/twitch/AddStreamer";
+import { authOptions, getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function AddStreamerPage() {
+export default async function AddStreamerPage() {
+
+  const session = await getAuthSession()
+
+  if (!session?.user) {
+    redirect(authOptions?.pages?.signIn || '/login')
+  }
+
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-2xl font-bold mb-4">Add Streamer</h1>
