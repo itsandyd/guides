69bac8cc47ad5bbac3a7ed540219f12a9de9cfdd
pauseplayrepeat <mrsqueex@gmail.com>
2024-06-02 19:11:17 -0500

    123

--- src/app/admin/create/page.tsx
+++ src/app/admin/create/page.tsx
@@ -1,5 +1,7 @@
 import { InitialForm } from "@/components/admin/initialForm";
 
+export const runtime = "edge"
+
 export default async function CreatePage() {
     return (
         <div className="mt-4 w-full">
